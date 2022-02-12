131dba9436c68a5d3e97850e4da426f013f287b6
korrawee <korrawee.somyos@gmail.com>
2022-02-12 11:45:05 +0700

    Add: income & outcome setting methods

--- app/models/summary.rb
+++ app/models/summary.rb
@@ -1,3 +1,29 @@
 class Summary < ApplicationRecord
-    belongs_to :wong
+    def setAllIncome(baans)
+        amount = 0
+        baans.each do |b|
+            b.getAllWong.each do |w|
+                amount += w.getTodayIncome()
+            end
+        end
+        puts '-------------getAllIncome-------------'
+        puts amount
+        puts '-------------getAllIncome-------------'
+        update!(income: amount, result: amount - outcome)
+    end
+
+    def setAllOutcome(baans)
+        amount = 0
+        baans.each do |b|
+            b.getAllWong.each do |w|
+                amount += w.getTodayPaid()
+            end
+        end
+        puts '-------------getAllIncome-------------'
+        puts amount
+        puts '-------------getAllIncome-------------'
+        update!(outcome: amount, result: income - amount)
+    end
+
+
 end
